82e6ecc84f459b7b208b44f26df3c620559e068f
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-01-25 20:35:08 -0500

    show an edit form

--- routes/todo.js
+++ routes/todo.js
@@ -13,9 +13,67 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/new', (req, res) => {
-      res.render('new');
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  if(typeof id != 'undefined') {
+    knex('todo')
+      .select()
+      .where('id', id)
+      .first()
+      .then(todo => {
+        console.log('todo', todo);
+        res.render('single', todo);
+      });
     
+  } else {
+    //respond with an error
+    res.status(500)
+    res.render('error', {
+      message: 'Invalid id'
+    });
+  }
+  
+});
+
+router.get('/new', (req, res) => {
+      res.render('new');  
+});
+
+router.get('/todo/:id/edit', (req, res) => {
+      res.render('edit');
+});
+
+ function validTodo(todo) {
+   return typeof todo.title == 'string' &&
+            todo.title.trim() != '' && 
+            typeof todo.priority != 'undefined' && !isNaN(Number(todo.priority));
+ }
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+      if(validTodo(req.body)){
+        // insert to the datbase
+        const todo = {
+          title: req.body.title,
+          description: req.body.description,
+          priority: req.body.priority,
+          date:new date()
+        };
+        
+        knex('todo')
+          .insert(todo, 'id')
+          .then(todos => {
+            const id = ids[0];
+            res.redirect(`/todo/${id}`);
+          });
+        
+      } else {
+        //respond with an error
+        res.status(500)
+        res.render('error', {
+          message: 'Invalid'
+        });
+      }
 });
 
 module.exports = router;
